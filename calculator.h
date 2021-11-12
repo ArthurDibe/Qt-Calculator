837fa7fd36998e76a037c7a439d9d1ee18de2127
Arthur Dibe <dibearthur@gmail.com>
2021-11-11 19:57:36 -0500

    Created functionalities: setEmpty, calculateResult, displayResult, button1 click, clearButton click. Created data members for result, current number, operation, and boolean to check the operation

---
+++ calculator.h
@@ -0,0 +1,35 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Calculator; }
+QT_END_NAMESPACE
+
+enum Operators {None, Sum, Subtract, Multiply, Divide};
+
+class Calculator : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Calculator(QWidget *parent = nullptr);
+    ~Calculator();
+
+private slots:
+    void setEmpty();
+    void calculateResult();
+    void displayResult();
+    void on_button1_clicked();
+    void on_clearButton_clicked();
+
+private:
+    Ui::Calculator *ui;
+    double result;
+    double number;
+    bool hasOperator;
+    Operators operation;
+
+};
+#endif // CALCULATOR_H
