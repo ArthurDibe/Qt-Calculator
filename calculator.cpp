783a52a87ec745328c35efb35ff6a118a193f463
Arthur Dibe <dibearthur@gmail.com>
2021-11-13 18:19:45 -0500

    Renamed variables, fixed the calculation

--- calculator.cpp
+++ calculator.cpp
@@ -8,7 +8,7 @@ Calculator::Calculator(QWidget *parent)
 {
     setEmpty();
     ui->setupUi(this);
-    ui->resultLabel->setText(QString::number(this->result));
+    ui->resultLabel->setText(QString::number(m_result));
     this->setFixedSize(350,391); // set fixed size
 }
 
@@ -21,59 +21,68 @@ Calculator::~Calculator()
 // Set Data members to empty state
 void Calculator::setEmpty()
 {
-    this->result = 0;
-    this->number = 0;
-    this->operation = Operators::None;
-    this->hasOperator = false;
-    this->numberString = "";
-    this->calculationString = "";
+    m_result = 0;
+    m_number = 0;
+    m_operation = Operators::None;
+    m_hasOperator = false;
+    m_numberString = "";
+    m_calculationString = "";
+    m_isFirstTime = true;
 }
 
 // function to calculate the result based on the operation selected
 void Calculator::calculateResult()
 {
-    switch(this->operation)
+    switch(m_operation)
     {
     case Operators::Sum:
-        this->result += this->number;
+        if(m_isFirstTime) m_result = m_number;
+        else    m_result += std::stod(m_numberString);
         break;
 
     case Operators::Subtract:
-        this->result -= this->number;
+        if(m_isFirstTime) m_result = m_number;
+        else    m_result -= std::stod(m_numberString);
         break;
 
     case Operators::Multiply:
-        this->result *= this->number;
+        if(m_isFirstTime) m_result = m_number;
+        else    m_result *= std::stod(m_numberString);
         break;
 
     case Operators::Divide:
-        this->result /= this->number;
+        if(m_isFirstTime) m_result = m_number;
+        else    m_result /= std::stod(m_numberString);
         break;
 
-    default:
-        this->result = 0;
+    case Operators::None:
+        break;
     }
+
+    m_isFirstTime = false;
 }
 
 // display the result data member on the calculator screen
 void Calculator::displayResult()
 {
-    ui->resultLabel->setText(QString::number(this->result));
+    ui->resultLabel->setText(QString::number(m_result));
 }
 
 // display the number data member on the calculator screen
 void Calculator::displayNumber()
 {
-    ui->resultLabel->setText(QString::number(this->number));
+    ui->resultLabel->setText(QString::fromStdString(m_numberString));
 }
 
 // display the result of the calculation and then set all the data members to empty state
 void Calculator::on_buttonEqual_clicked()
 {
-    if(std::isdigit(this->calculationString.back())){
-        this->calculationString += '=';
+    if(std::isdigit(m_calculationString.back())){
+        m_calculationString += '=';
         displayResult();
         updateCalculationLabel();
+        calculateResult();
+        displayResult();
         setEmpty(); // after calculating and displaying, reset data members
     }
 }
@@ -81,25 +90,15 @@ void Calculator::on_buttonEqual_clicked()
 // handle the clear button "CE"
 void Calculator::on_clearButton_clicked()
 {
-    this->setEmpty();
+    setEmpty();
     displayResult();
     updateCalculationLabel();
-}
-
-// check if an operator was selected before calculating the result
-// otherwise set the result to the value of the current number
-void Calculator::setOrCalculateResult()
-{
-    if(this->hasOperator)
-        this->calculateResult();
-    else
-        this->result = this->number;
 }
 
 // Check if a string has an operator as a last character
 // returns true if the last character is an operator
 // false otherwise
-bool Calculator::checkLastCharacter(const std::string obj_string)
+bool Calculator::checkLastForOperator(const std::string obj_string)
 {
     bool found = false;
     switch(obj_string.back())
@@ -124,170 +123,93 @@ bool Calculator::checkLastCharacter(const std::string obj_string)
 // Update the calculation label
 void Calculator::updateCalculationLabel()
 {
-    ui->calculationLabel->setText(QString::fromStdString(this->calculationString));
+    ui->calculationLabel->setText(QString::fromStdString(m_calculationString));
 }
 
+// This function was created to check if we need to replace the zero character (in case it is the first one)
 // returns true if the numberString is one length long and it has the character zero
 // false otherwise
 bool Calculator::hasOneAndZero()
 {
-    return (this->numberString.length() == 1) && (this->numberString.back() == '0');
+    return (m_numberString.length() == 1) && (m_numberString.back() == '0');
+}
+
+void Calculator::addNumber(char numberChosen)
+{
+    if(hasOneAndZero())
+        m_numberString.back() = numberChosen; // check if it is needed to replace the zero character
+    else
+        m_numberString += numberChosen;
+
+
+    if(checkLastForOperator(m_calculationString)) // check if there is an operator at last string position, in order to update the display
+        displayNumber();
+
+
+    m_calculationString += numberChosen;
+    updateCalculationLabel();
+    displayNumber();
+    //setOrCalculateResult();
+}
+
+void Calculator::addOperator(Operators opEnum, char opChar)
+{
+    m_operation = opEnum;
+    m_hasOperator = true;
+
+    if(checkLastForOperator(m_calculationString)){ // check if the operator should be replaced with another one or not
+        m_calculationString.back() = opChar;
+    }
+    else{
+        m_calculationString += opChar;
+    }
+
+    m_number = std::stod(m_numberString); // convert the numberSTring into double
+    m_numberString = "";                  // restart numberString
+    calculateResult();                    // update the result variable so far
+
+    updateCalculationLabel();
 }
 
 // handle the button 0
-void Calculator::on_button0_clicked()
-{
-    this->number = 0;
-    this->calculationString += "0";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button0_clicked(){ addNumber('0'); }
 
 // handle the button 1
-void Calculator::on_button1_clicked()
-{
-    this->number = 1;
-    this->calculationString += "1";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button1_clicked(){ addNumber('1'); }
 
 // handle the button 2
-void Calculator::on_button2_clicked()
-{
-    this->number = 2;
-    this->calculationString += "2";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button2_clicked(){ addNumber('2'); }
 
 // handle the button 3
-void Calculator::on_button3_clicked()
-{
-    this->number = 3;
-    this->calculationString += "3";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button3_clicked(){ addNumber('3'); }
 
 // handle the button 4
-void Calculator::on_button4_clicked()
-{
-    this->number = 4;
-    this->calculationString += "4";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button4_clicked(){ addNumber('4'); }
 
 // handle the button 5
-void Calculator::on_button5_clicked()
-{
-    this->number = 5;
-    this->calculationString += "5";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button5_clicked(){ addNumber('5'); }
 
 // handle the button 6
-void Calculator::on_button6_clicked()
-{
-    this->number = 6;
-    this->calculationString += "6";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button6_clicked(){ addNumber('6'); }
 
 // handle the button 7
-void Calculator::on_button7_clicked()
-{
-    this->number = 7;
-    this->calculationString += "7";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button7_clicked(){ addNumber('7'); }
 
 // handle the button 8
-void Calculator::on_button8_clicked()
-{
-    this->number = 8;
-    this->calculationString += "8";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button8_clicked(){ addNumber('8'); }
 
 // handle the button 9
-void Calculator::on_button9_clicked()
-{
-    this->number = 9;
-    this->calculationString += "9";
-    updateCalculationLabel();
-    displayNumber();
-    setOrCalculateResult();
-}
+void Calculator::on_button9_clicked(){ addNumber('9'); }
 
 // handle the button "+"
-void Calculator::on_buttonPlus_clicked()
-{
-    this->operation = Operators::Sum;
-    this->hasOperator = true;
-
-    if(checkLastCharacter(this->calculationString)){
-        this->calculationString.back() = '+';
-    }
-    else{
-        this->calculationString += "+";
-    }
-    updateCalculationLabel();
-}
+void Calculator::on_buttonPlus_clicked(){ addOperator(Operators::Sum, '+'); }
 
 // handle the button "-"
-void Calculator::on_buttonMinus_clicked()
-{
-    this->operation = Operators::Subtract;
-    this->hasOperator = true;
-    if(checkLastCharacter(this->calculationString)){
-        this->calculationString.back() = '-';
-    }
-    else{
-        this->calculationString += "-";
-    }
-    updateCalculationLabel();
-}
+void Calculator::on_buttonMinus_clicked(){ addOperator(Operators::Subtract, '-'); }
 
 // handle the button "*"
-void Calculator::on_buttonMultiply_clicked()
-{
-    this->operation = Operators::Multiply;
-    this->hasOperator = true;
-    if(checkLastCharacter(this->calculationString)){
-        this->calculationString.back() = 'x';
-    }
-    else{
-        this->calculationString += "x";
-    }
-    updateCalculationLabel();
-}
+void Calculator::on_buttonMultiply_clicked(){ addOperator(Operators::Multiply, 'x'); }
 
 // handle the button "/"
-void Calculator::on_buttonDivide_clicked()
-{
-    this->operation = Operators::Divide;
-    this->hasOperator = true;
-    if(checkLastCharacter(this->calculationString)){
-        this->calculationString.back() = '/';
-    }
-    else{
-        this->calculationString += "/";
-    }
-    updateCalculationLabel();
-}
+void Calculator::on_buttonDivide_clicked(){ addOperator(Operators::Divide, '/'); }
 
